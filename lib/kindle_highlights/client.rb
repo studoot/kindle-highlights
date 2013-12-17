cbcc4f80903c33d6aa625271d9bee6ef4c0583a8
Stuart Dootson <stuart.dootson@gmail.com>
2013-12-17 00:53:38 +0000

    Changed to use Books class, which is an enumerator, rather than gathering all the books in one go. Also mixin Enmerable to give standard enumerable methods

--- lib/kindle_highlights/client.rb
+++ lib/kindle_highlights/client.rb
@@ -1,4 +1,48 @@
+require 'ostruct'
+
 module KindleHighlights
+  def KindleHighlights.get_agent
+    agent = Mechanize.new
+    agent.user_agent_alias = 'Windows Mozilla'
+    agent.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    agent
+  end
+
+  class Books
+    include Enumerable
+
+    def initialize(email_address, password)
+      @email_address = email_address
+      @password = password
+    end
+
+    def each
+      agent = KindleHighlights::get_agent
+      signin_page = agent.get(KINDLE_LOGIN_PAGE)
+      
+      signin_form = signin_page.form(SIGNIN_FORM_IDENTIFIER)
+      signin_form.email = @email_address
+      signin_form.password = @password
+      
+      kindle_logged_in_page = agent.submit(signin_form)
+      books_page = agent.click(kindle_logged_in_page.link_with(text: /Your Books/))
+
+      loop do
+        books_page.search(".//td[@class='titleAndAuthor']").each do |book|
+          asin_and_title_element = book.search("a").first
+          asin = asin_and_title_element.attributes["href"].value.split("/").last
+          title = asin_and_title_element.inner_html
+          author = book.search("span[@class='author']").first.content.gsub(/[[:space:]]+/, ' ').strip
+          status = book.next_element.search(".//div[@class='statusText']/div[@class='text']").first.content.gsub(/[[:space:]]+/, ' ').strip.gsub(/ +/, '_')
+          yield OpenStruct.new({ :title => title, :asin => asin, :author => author, :status => status })
+        end
+        break if books_page.link_with(text: /Next/).nil?
+        books_page = agent.click(books_page.link_with(text: /Next/))
+      end
+    end
+
+  end
+
   class Client
     attr_reader :books
         
@@ -7,12 +51,11 @@ module KindleHighlights
       @password      = password
       @books         = Hash.new
       
-      setup_mechanize_agent
       load_books_from_kindle_account
     end
   
     def highlights_for(asin)
-      highlights = @mechanize_agent.get("https://kindle.amazon.com/kcw/highlights?asin=#{asin}&cursor=0&count=1000")
+      highlights = get_agent("https://kindle.amazon.com/kcw/highlights?asin=#{asin}&cursor=0&count=1000")
       json = JSON.parse(highlights.body)
       json["items"]
     end
@@ -20,33 +63,8 @@ module KindleHighlights
     private
    
     def load_books_from_kindle_account
-      signin_page = @mechanize_agent.get(KINDLE_LOGIN_PAGE)
-      
-      signin_form = signin_page.form(SIGNIN_FORM_IDENTIFIER)
-      signin_form.email = @email_address
-      signin_form.password = @password
-      
-      kindle_logged_in_page = @mechanize_agent.submit(signin_form)
-      highlights_page = @mechanize_agent.click(kindle_logged_in_page.link_with(text: /Your Books/))
+      Books.new(@email_address, @password).each { |b| @books[b.asin] = b }
+    end
 
-      loop do
-        highlights_page.search(".//td[@class='titleAndAuthor']").each do |book|
-          asin_and_title_element = book.search("a").first
-          asin = asin_and_title_element.attributes["href"].value.split("/").last
-          title = asin_and_title_element.inner_html
-          author = book.search("span[@class='author']").first.content.gsub(/[[:space:]]+/, ' ').strip
-          status = book.next_element.search(".//div[@class='statusText']/div[@class='text']").first.content.gsub(/[[:space:]]+/, ' ').strip.gsub(/ +/, '_')
-          @books[asin] = { :title => title, :asin => asin, :author => author, :status => status }
-        end
-        break if highlights_page.link_with(text: /Next/).nil?
-        highlights_page = @mechanize_agent.click(highlights_page.link_with(text: /Next/))
-      end
-    end
-    
-    def setup_mechanize_agent
-      @mechanize_agent = Mechanize.new
-      @mechanize_agent.user_agent_alias = 'Windows Mozilla'
-      @mechanize_agent.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    end
   end
 end
